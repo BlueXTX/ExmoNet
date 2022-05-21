ed1adc79a606f16e80bb14e8e063cac30a2f5c3f
BlueXT <bluextchannel@gmail.com>
2022-05-21 22:36:11 +0300

    Created ExmoNet.Cli project

---
+++ ExmoNet.Cli/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace ExmoNet.Cli;
+
+internal static class Program
+{
+    private static async Task Main(string[] args)
+    {
+        await Host.CreateDefaultBuilder()
+            .ConfigureServices(services => { })
+            .RunConsoleAppFrameworkAsync(args);
+    }
+}
